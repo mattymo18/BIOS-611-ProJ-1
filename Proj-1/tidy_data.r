a80819c0e47953c2c1e09c2fa1bbfe8b6e854f3f
Willa Burgess <dwj_wcb@imac.attlocal.net>
2020-08-27 16:58:34 -0400

    Cleaned up some of the data, made it 2000 with no NAs

--- Proj-1/tidy_data.r
+++ Proj-1/tidy_data.r
@@ -3,9 +3,19 @@ library(tidyverse)
 Draft <- read.csv("source_data/draft.csv")
 Combine <- read.csv("source_data/combine.csv")
 
-#do some work here
+#I'm thinking i'm going to use only data from 2000 on. I think these observations will be more complete. Also since the game has changed so much, this sort of analysis is only really relevant in the modern era of football. I also do not need a lot of these variables, lets select important ones 
+Comb.2000 <- Combine %>% 
+  filter(combineYear >= 2000) %>% 
+  select(1:6, 8:18, 28:33)
+Draft.2000 <- Draft %>% 
+  filter(draft >= 2000) %>% 
+  select(1:19)
+
+#ok lets take out any rows with an NA just to see
+Comb.compl <- na.omit(Comb.2000) 
+Draft.compl <- na.omit(Draft.2000)
 
 
-write.csv(Draft, "derived_data/draft.csv")
-write.csv(Combine, "derived_data/combine.csv")
+write.csv(Draft.compl, "derived_data/draft.csv")
+write.csv(Combine.compl, "derived_data/combine.csv")
 
