2e4e6ecad7c89e36a4a4906681e650c88f61237e
MATT JOHNSON <johnson.matt1818@gmail.com>
2020-09-08 11:18:47 -0400

    updated for adding graph

--- Makefile
+++ Makefile
@@ -3,9 +3,22 @@
 clean:
 	rm derived_data/*
 
-derived_data/Clean_Data.csv derived_data/combine.csv derived_data/draft.csv:\
+derived_data/Off.Skill.csv derived_data/Off.Strength.csv\
+derived_data/Def.Skill.csv dervied_data/Def.Strength.csv\
+derived_data/Clean_Data.csv\
+derived_data/combine.csv\
+derived_data/draft.csv:\
  source_data/combine.csv\
  source_data/draft.csv\
  tidy_data.r
 	Rscript tidy_data.r
 	
+	
+	
+derived_graphs/Off.Def.40s.png:\
+ derived_data/Def.Skill.csv\
+ derived_data/Def.Strength.csv\
+ derived_data/Off.Skill.csv\
+ derived_data/Off.Strength.csv\
+ tidy_graphs.r
+	Rscript tidy_graphs.r
--- tidy_data.r
+++ tidy_data.r
@@ -13,12 +13,30 @@ Draft.2000 <- Draft %>%
 
 #ok lets take out any rows with an NA just to see
 Comb.compl <- na.omit(Comb.2000) 
-Draft.compl <- na.omit(Draft.2000)
+Draft.compl <- na.omit(Draft.2000)[, c(1, 3, 4)]
 
 
 write.csv(Draft.compl, "derived_data/draft.csv")
-write.csv(Combine.compl, "derived_data/combine.csv")
+write.csv(Comb.compl, "derived_data/combine.csv")
 
 #join data into single clean df
 DF.clean <- na.omit(left_join(Comb.compl, Draft.compl, by = "playerId")[, -c(1, 3, 4:9, 11:13, 16)])
+DF.clean$position <- factor(DF.clean$position)
 write.csv(DF.clean, "derived_data/Clean_Data.csv")
+
+#split data into 4 groups
+DF.Off.skill <- DF.clean %>% 
+  filter(position == "WR" | position == "RB" | position == "TE")
+DF.Off.strength <- DF.clean %>% 
+  filter(position == "C" | position == "OG" | position == "OT" | position == "OL")
+DF.Def.skill <- DF.clean %>% 
+  filter(position == "DB" | position == "S")
+DF.Def.strength <- DF.clean %>% 
+  filter(position == "DE" | position == "DL" | position == "DT" | position == "LB" | position == "OLB")
+
+#save new csv's
+write.csv(DF.Off.skill, "derived_data/Off.Skill.csv")
+write.csv(DF.Off.strength, "derived_data/Off.Strength.csv")
+write.csv(DF.Def.skill, "derived_data/Def.Skill.csv")
+write.csv(DF.Def.strength, "derived_data/Def.Strength.csv")
+
